b4d995220db7a3b76b7fda23b90011b7295c112a
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-09-21 18:05:22 +0200

    Add fetch and print data on jsx

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import './App.css';
+import {fetchPokeData} from './services/fetchPokeData';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+ constructor(props) {
+  super(props);
+    this.state = {
+     pokeData: []
+    }
+  }
+
+  componentDidMount() {
+    this.getPokeDataFromApi()
+  }
+
+  getPokeDataFromApi() {
+    fetchPokeData()
+      .then(data => {
+        this.setState({
+          pokeData: data
+        })
+      })
   }
 
   render() {
+    console.log('me estoy pintando', this.state.pokeData)
     return (
       <div className="app">
-       
+        <ul className="poke-list">
+          {this.state.pokeData.map(pokeItem => 
+            <li key={pokeItem.id} className="poke-card">
+              <h2 className="poke-name">{pokeItem.name}</h2>
+              
+              <img src={pokeItem.url} alt="" className="poke-img"/>
+
+              {pokeItem.types.map(type=>
+                <p key={`${pokeItem.id}-type-${type}`} className="poke-type">{type}</p>
+              )}
+            </li>         
+          )}
+        </ul>
       </div>
     );
   }
---
+++ src/services/fetchPokeData.js
@@ -0,0 +1,8 @@
+const endpoint = 'https://raw.githubusercontent.com/Adalab/m3-evaluacion-intermedia-oneeyedman/componentbranch/pokemons.json';
+
+const fetchPokeData = () => {
+  return fetch(endpoint)
+  .then(response => response.json())
+}
+
+export {fetchPokeData};
